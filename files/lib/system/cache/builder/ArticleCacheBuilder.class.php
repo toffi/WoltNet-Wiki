ef06de0d3ccfc6ce4535a66766d28c28fa3f29e0
NurPech <nurpech@nurpech.de>
2013-03-29 22:19:48 +0100

    Signed-off-by: NurPech <nurpech@nurpech.de>

--- files/lib/system/cache/builder/ArticleCacheBuilder.class.php
+++ files/lib/system/cache/builder/ArticleCacheBuilder.class.php
@@ -16,24 +16,24 @@ use wcf\system\cache\builder\AbstractCacheBuilder;
  * @category 	WoltNet - Wiki
  */
 class ArticleCacheBuilder extends AbstractCacheBuilder {
-	/**
-	 * @see	wcf\system\cache\builder\AbstractCacheBuilder::rebuild()
-	 */
-	public function rebuild(array $parameters) {
-		$data = array(
-			'articles' => array()
-		);
+  /**
+   * @see	wcf\system\cache\builder\AbstractCacheBuilder::rebuild()
+   */
+  public function rebuild(array $parameters) {
+    $data = array(
+      'articles' => array()
+    );
 
-		$sql = "SELECT
-				articleID
-			FROM
-				wiki".WCF_N."_article";
-		$statement = WCF::getDB()->prepareStatement($sql);
-		$statement->execute();
-		while($row = $statement->fetchArray()) {
-			$data['articles'][$row['articleID']] = ViewableArticle::getArticle($row['articleID']);
-		}
+    $sql = "SELECT
+        articleID
+      FROM
+        wiki".WCF_N."_article";
+    $statement = WCF::getDB()->prepareStatement($sql);
+    $statement->execute();
+    while($row = $statement->fetchArray()) {
+      $data['articles'][$row['articleID']] = ViewableArticle::getViewableArticle($row['articleID']);
+    }
 
-		return $data;
-	}
+    return $data;
+  }
 }
