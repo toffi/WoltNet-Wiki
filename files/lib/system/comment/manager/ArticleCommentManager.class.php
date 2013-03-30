a2ca55b74ab896d746ccba68e44a312248f62f2b
NurPech <nurpech@nurpech.de>
2013-03-31 00:12:38 +0100

    added right for writing comments
    Signed-off-by: NurPech <nurpech@nurpech.de>

--- files/lib/system/comment/manager/ArticleCommentManager.class.php
+++ files/lib/system/comment/manager/ArticleCommentManager.class.php
@@ -1,5 +1,6 @@
 <?php
 namespace wiki\system\comment\manager;
+use wiki\data\article\Article;
 
 use wcf\system\comment\manager\AbstractCommentManager;
 use wcf\system\WCF;
@@ -12,38 +13,52 @@ use wcf\system\WCF;
  * @category 	WoltNet - Wiki
  */
 class ArticleCommentManager extends AbstractCommentManager {
+    public $article = null;
 
-	/**
-	 * @see wcf\system\SingletonFactory::init()
-	 */
-	protected function init() {
-	}
-	
-	/**
-	 * @see wcf\system\comment\manager\AbstractCommentManager::getLink()
-	 */
-	public function getLink($objectTypeID, $objectID) {
-		return "";
-	}
-	
-	/**
-	 * @see wcf\system\comment\manager\AbstractCommentManager::getTitle()
-	 */
-	public function getTitle($objectTypeID, $objectID, $isResponse = false) {
-		return "";
-	}
-	
-	/**
-	 * @see wcf\system\comment\manager\AbstractCommentManager::isAccessible()
-	 */
-	public function isAccessible($objectID) {
-		return true;
-	}
-	
-	/**
-	 * @see wcf\system\comment\manager\AbstractCommentManager::updateCounter()
-	 */
-	public function updateCounter($objectID, $value) {
-		
-	}
+    /**
+     * @see \wcf\system\comment\manager\AbstractCommentManager::canAdd()
+     */
+    public function canAdd($objectID) {
+        if($this->article === null) {
+        	$this->article = new Article($objectID);
+        }
+
+        if(!$this->isAccessible($objectID)) {
+            return false;
+        }
+
+        return $this->article->getPermission('canWriteComment');
+    }
+
+    /**
+     * @see wcf\system\comment\manager\AbstractCommentManager::getLink()
+     */
+    public function getLink($objectTypeID, $objectID) {
+      return "";
+    }
+
+    /**
+     * @see wcf\system\comment\manager\AbstractCommentManager::getTitle()
+     */
+    public function getTitle($objectTypeID, $objectID, $isResponse = false) {
+      return "";
+    }
+
+    /**
+     * @see wcf\system\comment\manager\AbstractCommentManager::isAccessible()
+     */
+    public function isAccessible($objectID) {
+    	if($this->article === null) {
+    		$this->article = new Article($objectID);
+    	}
+
+    	return $this->article->getPermission('canReadArticle');
+    }
+
+    /**
+     * @see wcf\system\comment\manager\AbstractCommentManager::updateCounter()
+     */
+    public function updateCounter($objectID, $value) {
+
+    }
 }
