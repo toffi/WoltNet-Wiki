c23f5c5ba97340ab93eb57c09dcfada37e55cb61
NurPech <nurpech@nurpech.de>
2013-04-20 14:27:06 +0200

    Signed-off-by: NurPech <nurpech@nurpech.de>

--- files/lib/system/category/WikiCategoryType.class.php
+++ files/lib/system/category/WikiCategoryType.class.php
@@ -7,23 +7,23 @@ class WikiCategoryType extends AbstractCategoryType {
 	/**
 	 * @see wcf\system\category\AbstractCategoryType::$permissionPrefix
 	 */
-	protected $permissionPrefix = 'admin.category';
-	
+	protected $permissionPrefix = 'admin.wiki.category';
+
 	/**
 	 * @see wcf\system\category\AbstractCategoryType::$langVarPrefix
 	 */
 	protected $langVarPrefix = 'wiki.category';
-	
+
 	/**
 	 * @see wcf\system\category\AbstractCategoryType::$objectTypes
 	 */
 	protected $objectTypes = array('com.woltlab.wcf.acl' => 'com.woltnet.wiki.category');
-	
+
 	/**
 	 * @see wcf\system\category\AbstractCategoryType::$forceDescription
 	 */
 	protected $forceDescription = false;
-	
+
 	/**
 	* @see  wcf\system\category\ICategoryType::getApplication()
 	*/
