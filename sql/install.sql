427bc00cff2ed6305496fdf34443f62b434da9d0
NurPech <nurpech@nurpech.de>
2013-04-30 18:38:49 +0200

    new database structure
    Signed-off-by: NurPech <nurpech@nurpech.de>

--- sql/install.sql
+++ sql/install.sql
@@ -2,23 +2,29 @@
 DROP TABLE IF EXISTS wiki1_article;
 CREATE TABLE wiki1_article (
 	articleID 	INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	categoryID 	INT(10),
-	userID		INT(10),
-	username	VARCHAR(255),
-	subject		VARCHAR(255) NOT NULL,
-	message		TEXT,
-	time 		INT(10) NOT NULL,
-	languageID   	INT(10),
+	categoryID 	INT(10) NOT NULL,
+	versionID       INT(10) NOT NULL,
+	languageID   	INT(10) NOT NULL,
 	translationID	INT(10) NOT NULL,
-	parentID	INT DEFAULT NULL,
-	isActive	TINYINT(1) NOT NULL DEFAULT 0,
-	isDeleted	TINYINT(1) NOT NULL DEFAULT 0,
-	deleteTime 	INT(10) NULL,
-	lastPostTime	INT(10),
-	enableSmilies 	TINYINT(1) NOT NULL DEFAULT 1,
-	enableHtml 	TINYINT(1) NOT NULL DEFAULT 0,
-	enableBBCodes	TINYINT(1) NOT NULL DEFAULT 1
-	ipAddress 	VARCHAR(39) NOT NULL DEFAULT ''
+);
+
+DROP TABLE IF EXISTS wiki1_article_version;
+CREATE TABLE wiki1_article_version (
+    versionID   INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    articleID  INT(10) NOT NULL,
+    userID      INT(10),
+    username    VARCHAR(255),
+    subject     VARCHAR(255) NOT NULL,
+    message     TEXT,
+    time        INT(10) NOT NULL,
+    parentID    INT DEFAULT NULL,
+    isActive    TINYINT(1) NOT NULL DEFAULT 0,
+    isDeleted   TINYINT(1) NOT NULL DEFAULT 0,
+    deleteTime  INT(10) NULL,
+    enableSmilies   TINYINT(1) NOT NULL DEFAULT 1,
+    enableHtml  TINYINT(1) NOT NULL DEFAULT 0,
+    enableBBCodes   TINYINT(1) NOT NULL DEFAULT 1
+    ipAddress   VARCHAR(39) NOT NULL DEFAULT ''
 );
 
 -- category suggestion
@@ -51,8 +57,9 @@ CREATE TABLE wiki1_article_label_to_object (
 
 -- foreign keys
 ALTER TABLE wiki1_article ADD FOREIGN KEY (categoryID) REFERENCES wcf1_category (categoryID) ON DELETE CASCADE;
-ALTER TABLE wiki1_article ADD FOREIGN KEY (languageID) REFERENCES wcf1_language (languageID) ON DELETE SET NULL;
-ALTER TABLE wiki1_article ADD FOREIGN KEY (userID) REFERENCES wcf1_user (userID) ON DELETE SET NULL;
+ALTER TABLE wiki1_article ADD FOREIGN KEY (languageID) REFERENCES wcf1_language (languageID) ON DELETE CASCADE;
+ALTER TABLE wiki1_article_version ADD FOREIGN KEY (userID) REFERENCES wcf1_user (userID) ON DELETE SET NULL;
+ALTER TABLE wiki1_article_version ADD FOREIGN KEY (articleID) REFERENCES wiki1_article (articleID) ON DELETE CASCADE;
 
 ALTER TABLE wiki1_article_label ADD FOREIGN KEY (categoryID) REFERENCES wcf1_category (categoryID) ON DELETE CASCADE;
 
